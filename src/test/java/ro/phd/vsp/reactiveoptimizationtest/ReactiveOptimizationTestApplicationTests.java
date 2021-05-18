ecca72903cc9c4d09793037140a5123f98b89230
VladPetre <vladstefan.petre@gmail.com>
2021-05-18 21:10:29 +0300

    initial commit - main project

---
+++ src/test/java/ro/phd/vsp/reactiveoptimizationtest/ReactiveOptimizationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.phd.vsp.reactiveoptimizationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveOptimizationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
