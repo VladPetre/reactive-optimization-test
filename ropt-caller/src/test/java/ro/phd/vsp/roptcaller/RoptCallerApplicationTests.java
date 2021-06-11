3058b2e2d7d75f624ab69f1e94c78355fcb7cdb7
VladPetre <vladstefan.petre@gmail.com>
2021-06-11 17:47:56 +0300

    fix peckages for caller

--- ropt-caller/src/test/java/ro/phd/vsp/roptcaller/RoptCallerApplicationTests.java
+++ ropt-caller/src/test/java/ro/phd/vsp/roptcaller/RoptCallerApplicationTests.java
@@ -1,4 +1,4 @@
-package ro.phd.vsp.roptcallerreactive;
+package ro.phd.vsp.roptcaller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RoptCallerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
