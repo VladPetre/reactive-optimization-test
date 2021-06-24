800f834cb840cb501e3d49f9dfd4e64d064a7e27
VladPetre <vladstefan.petre@gmail.com>
2021-06-25 02:14:05 +0300

    replace restcontroller with controller in reactive receiver

--- ropt-receiver-reactive/src/main/java/ro/phd/vsp/roptreceiverreactive/controllers/MainController.java
+++ ropt-receiver-reactive/src/main/java/ro/phd/vsp/roptreceiverreactive/controllers/MainController.java
@@ -2,18 +2,18 @@ package ro.phd.vsp.roptreceiverreactive.controllers;
 
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import ro.phd.vsp.roptreceiverreactive.dtos.SensorDataDTO;
 import ro.phd.vsp.roptreceiverreactive.services.SensorDataService;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping(path = "/rt/sensor-data")
 public class MainController {
