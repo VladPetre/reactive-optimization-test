8dc4be831195bc2ec2a624b89c16b928c7f99ee0
VladPetre <vladstefan.petre@gmail.com>
2021-06-28 23:10:04 +0300

    fix body for updateWithGet

--- ropt-receiver-reactive/src/main/java/ro/phd/vsp/roptreceiverreactive/handlers/SensorDataHandler.java
+++ ropt-receiver-reactive/src/main/java/ro/phd/vsp/roptreceiverreactive/handlers/SensorDataHandler.java
@@ -57,7 +57,7 @@ public class SensorDataHandler {
         .flatMap(data ->
             created(UriComponentsBuilder.fromPath("rt/sensor-data").build().toUri())
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(data, SensorDataDTO.class)
+                .bodyValue(data)
         );
   }
 }
